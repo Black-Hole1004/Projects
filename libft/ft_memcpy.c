cc395fde3b321f462c77d64be8cd98b35e82849c
Abderrahman Hmaymou <ahmaymou@e2r1p5.1337.ma>
2022-10-23 11:37:49 +0100

    auto

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ahmaymou <ahmaymou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 14:04:05 by ahmaymou          #+#    #+#             */
-/*   Updated: 2022/10/21 19:33:29 by ahmaymou         ###   ########.fr       */
+/*   Updated: 2022/10/23 11:31:49 by ahmaymou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	char	*d;
 	char	*temp;
 
-	if (!dst && !src)
-		return (NULL);
 	s = (char *)src;
 	d = (char *)dst;
 	temp = (char *)dst;
