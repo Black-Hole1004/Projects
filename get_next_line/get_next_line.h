a445e14d86b4a07ccb95ed6df18998329fc3cf8c
Abderrahman Hmaymou <ahmaymou@e2r9p15.1337.ma>
2022-11-15 22:08:48 +0100

    auto

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,16 +6,15 @@
 /*   By: ahmaymou <ahmaymou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 16:18:08 by ahmaymou          #+#    #+#             */
-/*   Updated: 2022/11/15 21:22:33 by ahmaymou         ###   ########.fr       */
+/*   Updated: 2022/11/15 21:36:05 by ahmaymou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
